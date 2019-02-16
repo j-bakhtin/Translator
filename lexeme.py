0e476013f426604d2377c3961c8ddb54e5703944
Jurij Bakhtin <jurij.bakhtin@gmail.com>
2019-02-17 02:10:27 +0700

    [refacroting]all code

--- lexeme.py
+++ lexeme.py
@@ -1,42 +1,40 @@
-'''
-Created on 4 Feb. 2019 y.
 
-@author: Jurij Bakhtin
-'''
+# Created on 4 Feb. 2019 y.
+# @author: Jurij Bakhtin
+
 from builtins import str
-from itertools import islice, count
 import sys
 import os
 
 # -*- Символы ограничители
-limeters = {
-            '(':'LRB',
-            ')':'RRB',
-            '[':'LSB', 
-            ']':'RSB', 
-            '{':'LCB', 
-            '}':'RCB',
-            ':':'Colon',
-            ',':'Comma',
-            ';':'Semicolon',}
+limiters = {
+            '(': 'LRB',
+            ')': 'RRB',
+            '[': 'LSB',
+            ']': 'RSB',
+            '{': 'LCB',
+            '}': 'RCB',
+            ':': 'Colon',
+            ',': 'Comma',
+            ';': 'Semicolon'}
 
 # Зарезервированные операторы
 reserved_operators = {
-            '+':'Add',
-            '-':'Min',
-            '*':'Mul',
-            '<':'LT',
-            '>':'GT'}
+            '+': 'Add',
+            '-': 'Min',
+            '*': 'Mul',
+            '<': 'LT',
+            '>': 'GT'}
 
 # Зарезервированные составные операторы
 reserved_composite_operators = {
-            '==':'EQ',
-            '!=':'NE',
-            '<':'LT',
-            '>':'GT',
-            '<=':'LE',
-            '>=':'GE',
-            ':=':'Let', }
+            '==': 'EQ',
+            '!=': 'NE',
+            '<': 'LT',
+            '>': 'GT',
+            '<=': 'LE',
+            '>=': 'GE',
+            ':=': 'Let'}
 
 # -*- Зарезервированные слова
 reserved_words = ['Div', 'Mod', 'Cast', 'Box','End', 'Int', 'Vector', 'TypeInt', 'TypeReal', 'Goto', 'Read',
@@ -52,26 +50,29 @@ reserved_words = ['Div', 'Mod', 'Cast', 'Box','End', 'Int', 'Vector', 'TypeInt',
 #             'Tab'
 
 
-
 # -*- Класс, описывающий лексему в формате <номер строки>lex:<лексема>[<тип>:<распознанное_значение>][val:<значение>]
-class Lexeme:  
-        
-    def __init__(self, lineNumber, value, error = False):
-        self.lineNumber = str(lineNumber)
+class Lexeme:
+    line_number = None
+    value = None
+    exception = None
+
+    def __init__(self, line_number, value, exception=None):
+        self.lineNumber = str(line_number)
         self.value = str(value)
-        self.error = error
+        self.error = exception
     
     # -*- Полное описание лексемы -*-
-    def getDescription(self):
-        description = self.lineNumber
-        
-        
-        if self.error == True:
+    def get_description(self):
+        description = self.line_number
+
+        if self.exception == 'Error':
             description += 'lex:' + 'Error' + 'val:' + self.value
+        elif self.exception == 'Comment':
+            description += 'lex:' + 'Comment'
         elif self.value in reserved_words:
             description += 'lex:' + self.value + 'val:' + self.value
-        elif self.value in limeters.keys():
-            description += 'lex:' + limeters.get(self.value) + 'val:' + self.value
+        elif self.value in limiters.keys():
+            description += 'lex:' + limiters.get(self.value) + 'val:' + self.value
         elif self.value in reserved_operators.keys():
             description += 'lex:' + reserved_operators.get(self.value) + 'val:' + self.value
         elif self.value in reserved_composite_operators.keys():
@@ -79,219 +80,193 @@ class Lexeme:
         else:
             try: 
                 if type(int(self.value)) is int:
-                    description += 'lex:' + 'TypeInt' + type(int(self.value)).__name__ + ':' + self.value + 'val:' + self.value
-                    return(description)
+                    description += 'lex:' + 'TypeInt' + type(int(self.value)).__name__ +\
+                                   ':' + self.value + 'val:' + self.value
+                    return description
             except ValueError:   
                 pass
                 
             try: 
                 if type(float(self.value)) is float:
-                    description += 'lex:' + 'TypeRal' + type(float(self.value)).__name__ + ':' + self.value + 'val:' + self.value
-                    return(description)
+                    description += 'lex:' + 'TypeRal' + type(float(self.value)).__name__ +\
+                                   ':' + self.value + 'val:' + self.value
+                    return description
             except ValueError:   
-                description += 'lex:' + 'Id' +'val:' + self.value
-        
-        return(description) 
-    
-    
-    def getDescriptionTable(self):
-        return("Номер строки: " + self.lineNumber + '\nЛексема: ' + self.lexeme + '\nТип: ' + self.typeLexeme + 
-               '\nРАспознанное значение: ' + self.recognizedValue + '\nЗначение: ' + self.value) 
+                description += 'lex:' + 'Id' + 'val:' + self.value
+
+        return description
 
 
 # -*- Определение принадлежности символа к классу букв -*-
-def isLetter(ch):
-    if (ch >= 'A' and ch <= 'Z') or (ch >= 'a' and ch <= 'z'):
+def is_letter(ch):
+    if ('A' <= ch <= 'Z') or ('a' <= ch <= 'z'):
         return True
     else:
         return False
 
   
 # -*- Определение принадлежности символа к классу двоичных цифр -*-
-def isBin(ch):
-    if((ch == '0' or ch == '1')):
+def is_bin(ch):
+    if ch == '0' or ch == '1':
         return True
     else:
         return False
 
    
 # -*- Определение принадлежности символа к классу восьмиричных цифр -*- 
-def isOctal(ch):
-    if((ch >= '0' and ch <= '7')):
+def is_octal(ch):
+    if '0' <= ch <= '7':
         return True
     else:
         return False
 
 
 # -*- Определение принадлежности символа к классу десятичных цифр -*-
-def isDigit(ch):
-    if((ch >= '0' and ch <= '9')):
+def is_digit(ch):
+    if '0' <= ch <= '9':
         return True
     else:
         return False
 
 
 # -*- Определение принадлежности символа к классу шестнадцатеричныхцифр -*-
-def isHex(ch):
-    if((ch >= '0' and ch <= '9') or (ch >= 'A' and ch <= 'F') or (ch >= 'a' and ch <= 'f')):
+def is_hex(ch):
+    if ('0' <= ch <= '9') or ('A' <= ch <= 'F') or ('a' <= ch <= 'f'):
         return True
     else:
         return False
     
 
 # -*-Определение принадлежности символа к классу пропусков -*-
-def isSkip(ch):
-    if(ch == ' ' or ch == '\t' or ch == '\n' or ch == '\f'):
+def is_skip(ch):
+    if ch == ' ' or ch == '\t' or ch == '\n' or ch == '\f':
         return True
     else:
         return False
+
 
 # -*- Определяет принадлежность к классу игнорируемых символов -*-
-def isIgnore(ch):
-    if (ch > chr(0) and ch < ' ' and ch != '\t' and ch != '\n' and ch !='\f'):
+def is_ignore(ch):
+    if chr(0) < ch < ' ' and ch != '\t' and ch != '\n' and ch != '\f':
         return True
     else:
         return False
 
 
-def isReserved(ch):
+def is_reserved(ch):
     return ch in reserved_words
 
 
-def isLimeters(ch):
-    return ch in limeters
+def is_limiters(ch):
+    return ch in limiters
 
 
-def transliterator(ch):
-    pass
+# ===============================================================================
+# Лексический анализатор (Сканер)
+# Принимает фал с программой
+# Возвращает список сописанием лексем
+# Формат описания лексемы: <номер строки>lex:<лексема>[<тип>:<распознанное_значение>][val:<значение>]
+# ===============================================================================
+def scanner(file_program):
+    obj_list = []
 
-
-# -*- Лексический анализатор. Принимает целый файл-*-
-def scanner(file_programm):
-    obj_list = [] # Список лексем-объектов
-    lexem = ''  # Обрабатываемая лексема
-    # Разбиваем вхожной поток по строкам
-    for line_number, line in enumerate(file_programm):
-        index_in_line = 0 # Индекс обрабатываемого символа, обнуляем для каждой строки
-        lexem = '' # обнуляем лексему
-        # Блок транслитератор?!    <<<---
-        # Начинаем перебор символов
+    for line_number, line in enumerate(file_program):
+        index_in_line = 0
+        lexeme = ''
         while index_in_line < len(line):
-            # Тип лексемы
-            error = False
-            
-            if isSkip(line[index_in_line]):
-                index_in_line +=1
+            exception = False
+
+            if is_skip(line[index_in_line]):
+                index_in_line += 1
                 continue
             
-            if isLimeters(line[index_in_line]):
-                lexem = lexem + line[index_in_line]
-                obj = Lexeme(line_number,lexem)
+            if is_limiters(line[index_in_line]):
+                lexeme = lexeme + line[index_in_line]
+                obj = Lexeme(line_number, lexeme)
                 obj_list.append(obj)
-                lexem = ''
-                index_in_line +=1
+                lexeme = ''
+                index_in_line += 1
                 continue
                     
-            # Если БУКВА, то начинаем обрабатывать возможные лексемы:
-            #  - ЗАРЕЗЕРВИРОВАННАЯ ИНСТРУКЦИЯ
-            #  - ИДЕНТИФИКАТОР
-            #  - ... Дописать    <<<---
-            if isLetter(line[index_in_line]):
-                # Заполняем лексему 
-                lexem = lexem + line[index_in_line]
-                # Обрабатываем дальш потенциальную лексему
-                index_in_lexem = index_in_line + 1
-                while index_in_lexem < len(line):
-                    # Если последующий символ БУКВА то
-                    if isLetter(line[index_in_lexem]):
+            if is_letter(line[index_in_line]):
+                lexeme = lexeme + line[index_in_line]
+                index_in_lexeme = index_in_line + 1
+                while index_in_lexeme < len(line):
 
-                        # Заполняем лесему
-                        lexem = lexem + line[index_in_lexem]
-                        # Продолжаем обработку возможной лексемы
-                        index_in_lexem += 1
-                        continue
-                    elif isDigit(line[index_in_lexem]):
-
-                        # Заполняем лесему
-                        lexem = lexem + line[index_in_lexem]
-                        # Продолжаем обработку возможной лексемы
-                        index_in_lexem += 1
+                    if is_letter(line[index_in_lexeme]):
+                        lexeme = lexeme + line[index_in_lexeme]
+                        index_in_lexeme += 1
                         continue
                     
-                    elif line[index_in_lexem] == '_':
-                        lexem = lexem + line[index_in_lexem]
-                        index_in_lexem += 1
+                    elif is_digit(line[index_in_lexeme]):
+                        lexeme = lexeme + line[index_in_lexeme]
+                        index_in_lexeme += 1
+                        continue
+                    
+                    elif line[index_in_lexeme] == '_':
+                        lexeme = lexeme + line[index_in_lexeme]
+                        index_in_lexeme += 1
                         continue
                         
-                    # Иначе, если разделяющий символ, то определена лексема (ИДЕНТИФИКАТОР или ЗАРЕЗЕРВИРОВАННАЯ ИНСТРУКЦИЯ)
-                    elif isSkip(line[index_in_lexem]):
-                        # Добавляем лексему в список
-                        if isReserved(lexem.lower()):
-                            obj = Lexeme(line_number, lexem)
+                    elif is_skip(line[index_in_lexeme]):
+                        if is_reserved(lexeme.lower()):
+                            obj = Lexeme(line_number, lexeme)
                             obj_list.append(obj)
-                        else:
-                            obj = Lexeme(line_number, lexem)
-                        # Обнуляем обрабатываемую лексему
-                        index_in_line = index_in_lexem
-                        lexem = ''
-                        # Заканчиваем обраьотку лесемы
+                        index_in_line = index_in_lexeme
+                        lexeme = ''
                         break
                     
-                    elif isLimeters(line[index_in_lexem]):
-                        obj = Lexeme(line_number, lexem)
+                    elif is_limiters(line[index_in_lexeme]):
+                        obj = Lexeme(line_number, lexeme)
                         obj_list.append(obj)
-                        lexem = ''
-                        index_in_line = index_in_lexem
+                        lexeme = ''
+                        index_in_line = index_in_lexeme
                         break
                     
-                    # Если символ не определен, то предположение о возможной лексеме ложно
                     else:
-                        # Обнуляем обрабатываемую лесему
-                        lexem = ''
-                        # Заканчиваем обраьотку лесемы
-                        index_in_line = index_in_lexem
+                        lexeme = ''
+                        index_in_line = index_in_lexeme
                         break
-                # Выходим из цикла, чтобы начать обработку новой, потенциальной, лексемы
                 continue
             
-            if isDigit(line[index_in_line]):
-                lexem = lexem + line[index_in_line]
-                index_in_lexem = index_in_line + 1
+            if is_digit(line[index_in_line]):
+                lexeme = lexeme + line[index_in_line]
+                index_in_lexeme = index_in_line + 1
                 
-                while index_in_lexem < len(line): 
-                    if isDigit(line[index_in_lexem]):
-                        lexem = lexem + line[index_in_lexem]
-                        index_in_lexem += 1
+                while index_in_lexeme < len(line):
+                    if is_digit(line[index_in_lexeme]):
+                        lexeme = lexeme + line[index_in_lexeme]
+                        index_in_lexeme += 1
                         continue
                     
-                    elif isLetter(line[index_in_lexem]):
-                        error = True
-                        lexem = lexem + line[index_in_lexem]
-                        index_in_lexem += 1
+                    elif is_letter(line[index_in_lexeme]):
+                        exception = 'Error'
+                        lexeme = lexeme + line[index_in_lexeme]
+                        index_in_lexeme += 1
                         continue
                     
-                    elif line[index_in_lexem] == '.':
-                        lexem = lexem + line[index_in_lexem]
-                        index_in_lexem += 1
+                    elif line[index_in_lexeme] == '.':
+                        lexeme = lexeme + line[index_in_lexeme]
+                        index_in_lexeme += 1
                         continue 
                       
-                    elif isSkip(line[index_in_lexem]):
-                        obj = Lexeme(line_number, lexem, error)
+                    elif is_skip(line[index_in_lexeme]):
+                        obj = Lexeme(line_number, lexeme, exception)
                         obj_list.append(obj)
-                        lexem = ''
-                        index_in_line = index_in_lexem
+                        lexeme = ''
+                        index_in_line = index_in_lexeme
                         break
                     
-                    elif isLimeters(line[index_in_lexem]):
-                        obj = Lexeme(line_number, lexem, error)
+                    elif is_limiters(line[index_in_lexeme]):
+                        obj = Lexeme(line_number, lexeme, exception)
                         obj_list.append(obj)
-                        lexem = ''
-                        index_in_line = index_in_lexem
+                        lexeme = ''
+                        index_in_line = index_in_lexeme
                         break
                     
                     else:
-                        lexem = ''
-                        index_in_line = index_in_lexem
+                        lexeme = ''
+                        index_in_line = index_in_lexeme
                         break
                     
                 index_in_line += 1
@@ -305,26 +280,24 @@ def scanner(file_programm):
 # -*- Главная функция -*-
 def main(fp, fl):
     obj_list = []
-    
-    
+
     if len(sys.argv) != 1:
         if os.stat(sys.argv[1]).st_size != 0:
-            with open(fp) as input_file_programm:
-                obj_list = scanner(input_file_programm)
+            with open(fp) as input_file_program:
+                obj_list = scanner(input_file_program)
         else:
             print("Input file is empty")
     else:
-        print('Parametrs not found')
+        print('Parameters not found')
         
     for obj in obj_list:
-        print(obj.getDescription())
+        print(obj.get_description())
         
     input('Enter ...')
 
 
 if __name__ == '__main__':
-    if sys.argv != []:
+    if sys.argv.__sizeof__ != 0:
         main(sys.argv[1], sys.argv[2])
     else:
-        main('fp2.txt', 'fl2.txt')
-
+        main('tp2.txt', 'tl2.txt')
